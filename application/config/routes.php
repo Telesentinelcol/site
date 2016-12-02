5146941940c622fa3b89cb176c2f03e7287adc89
Edison Ospina <edisonv16@hotmail.com>
2016-12-02 15:35:34 -0500

    optimización de sitio web
    Páginas que no se utilizan

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,44 +1,46 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 /*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	http://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There area two reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router what URI segments to use if those provided
-| in the URL cannot be matched to a valid route.
-|
-*/
+  | -------------------------------------------------------------------------
+  | URI ROUTING
+  | -------------------------------------------------------------------------
+  | This file lets you re-map URI requests to specific controller functions.
+  |
+  | Typically there is a one-to-one relationship between a URL string
+  | and its corresponding controller class/method. The segments in a
+  | URL normally follow this pattern:
+  |
+  |	example.com/class/method/id/
+  |
+  | In some instances, however, you may want to remap this relationship
+  | so that a different class/function is called than the one
+  | corresponding to the URL.
+  |
+  | Please see the user guide for complete details:
+  |
+  |	http://codeigniter.com/user_guide/general/routing.html
+  |
+  | -------------------------------------------------------------------------
+  | RESERVED ROUTES
+  | -------------------------------------------------------------------------
+  |
+  | There area two reserved routes:
+  |
+  |	$route['default_controller'] = 'welcome';
+  |
+  | This route indicates which controller class should be loaded if the
+  | URI contains no data. In the above example, the "welcome" class
+  | would be loaded.
+  |
+  |	$route['404_override'] = 'errors/page_missing';
+  |
+  | This route will tell the Router what URI segments to use if those provided
+  | in the URL cannot be matched to a valid route.
+  |
+ */
 $route['default_controller'] = "home";
-/*Ciudades*/
+/* Ciudades */
 $route['home'] = 'home/';
 $route['televideo'] = 'home/section/$1/';
 $route['rastreo'] = 'home/section/$1/';
@@ -52,12 +54,8 @@ $route['tunja'] = 'home/section/$1/';
 $route['valledupar'] = 'home/section/$1/';
 $route['monitoreo'] = 'home/section/$1/';
 $route['equipos'] = 'home/section/$1/';
-$route['oferta'] = 'home/section/$1/';
-$route['cambiese'] = 'home/section/$1/';
-$route['referido'] = 'home/section/$1/';
 $route['sitemap'] = 'home/section/$1/';
 $route['error'] = 'home/section/$1/';
-$route['blackfriday'] = 'home/section/$1/';
 $route['sensor-doble-tecnologia'] = 'home/productos/$1/';
 $route['magneticos'] = 'home/productos/$1/';
 $route['teclado'] = 'home/productos/$1/';
@@ -66,9 +64,9 @@ $route['sirena'] = 'home/productos/$1/';
 $route['radio'] = 'home/productos/$1/';
 $route['vibro-sensor'] = 'home/productos/$1/';
 
-/*Ofertas*/
+/* Ofertas */
 
-/*Adwords*/
+/* Adwords */
 $route['busqueda'] = "busqueda/index/";
 $route['busqueda/(:any)'] = 'busqueda/section/$1/';
 
@@ -91,7 +89,7 @@ $route['mailing'] = "mailing/index/";
 $route['mailing/(:any)'] = 'mailing/section/$1/';
 
 $route['gracias'] = 'gracias/index/';
-/*Pagina de error*/
+/* Pagina de error */
 $route['404_override'] = 'home/error';
 
 /* End of file routes.php */
