05c1aec3b1e13db5a99246f5539e31d2c9ba24f6
scorpio <scorpio@scorpio.telesentinel.info>
2016-12-22 15:36:31 -0500

    [TelesentinelSite] controller Blog

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,7 +1,8 @@
 <?php
 
-if (!defined('BASEPATH')) 
+if (!defined('BASEPATH'))
     exit('No direct script access allowed');
+
 /**
  * Description of adwords
  *
@@ -30,12 +31,12 @@ class Home extends CI_Controller {
         $this->load->view('home');
         $this->load->view('footer');
     }
-    
-    public function section(){
-        
+
+    public function section() {
+
         $path = explode("/", $_SERVER['REQUEST_URI']);
-        
-        if($path[1] == 'telesentinel') :
+
+        if ($path[1] == 'telesentinel') :
             $section = $path[2];
         else :
             $section = $path[1];
@@ -45,58 +46,102 @@ class Home extends CI_Controller {
         $data['section'] = '../';
         $data['equipos'] = '../';
         $data['inicio'] = '../';
-        
+
         $this->load->view('head', $data);
         $this->load->view('menu', $data);
 
         $permitidos = $this->Permitidos();
 
-        if(in_array($section, $permitidos)) {
-            $this->load->view('section/'.$section, $data);
-        }
-        else{
+        if (in_array($section, $permitidos)) {
+            $this->load->view('section/' . $section, $data);
+        } else {
             $this->load->view('section/error');
         }
 
         $this->load->view('footer', $data);
     }
 
-    public function productos(){         
-        
+    public function blog() {
+
         $path = explode("/", $_SERVER['REQUEST_URI']);
-        
-        if($path[1] == 'telesentinel') :
+
+        if ($path[1] == 'telesentinel') :
             $section = $path[2];
         else :
             $section = $path[1];
         endif;
+
+        $data['rut'] = '../';
+        $data['section'] = '../';
+
+        $this->load->view('head', $data);
+        $this->load->view('menu', $data);
+
+        $this->load->database();
         
+        $sql = 'SELECT * FROM logblog WHERE Estado = ? ORDER BY Fecha DESC LIMIT 9';
+        $query = $this->db->query($sql, array(1));
+        
+        $sql_aux = 'SELECT * FROM logblog ORDER BY Calificacion DESC LIMIT 3';
+        $query_aux = $this->db->query($sql_aux);
+         
+        if($query != NULL) :
+            $data['registros'] = $query;
+        else :
+            $data['registros'] = array();
+        endif;
+        
+        if($query_aux != NULL) :
+            $data['registros_aux'] = $query_aux;
+        else :
+            $data['registros_aux'] = array();
+        endif;
+        
+        $permitidos = $this->Permitidos();
+
+        if (in_array($section, $permitidos)) {
+            $this->load->view('blog/' . $section, $data);
+        } else {
+            $this->load->view('section/error');
+        }
+
+        $this->load->view('footer', $data);
+    }
+
+    public function productos() {
+
+        $path = explode("/", $_SERVER['REQUEST_URI']);
+
+        if ($path[1] == 'telesentinel') :
+            $section = $path[2];
+        else :
+            $section = $path[1];
+        endif;
+
         $data['rut'] = '../';
         $data['section'] = '../';
         $data['equipos'] = '../';
         $data['inicio'] = '../';
-        
+
         $this->load->view('head', $data);
         $this->load->view('menu', $data);
 
-        $permitidos = array('sensor-doble-tecnologia','magneticos','teclado','sensor-exterior','boton-panico','sirena','radio','sensor-lunar','sensor-humo','magneticos-metalicos','dvr','discriminador-audio','barrerasfotoelectricas','camara-domo','camara');
+        $permitidos = array('sensor-doble-tecnologia', 'magneticos', 'teclado', 'sensor-exterior', 'boton-panico', 'sirena', 'radio', 'sensor-lunar', 'sensor-humo', 'magneticos-metalicos', 'dvr', 'discriminador-audio', 'barrerasfotoelectricas', 'camara-domo', 'camara');
 
-        if(in_array($section, $permitidos)) {
-            $this->load->view('productos/'.$section, $data);
-        }
-        else{
+        if (in_array($section, $permitidos)) {
+            $this->load->view('productos/' . $section, $data);
+        } else {
             $this->load->view('section/error');
-            
         }
 
         $this->load->view('footer', $data);
-    }  
-    
-    public function error()
-    {
+    }
+
+    public function error() {
         $data['rut'] = '../';
         $this->load->view('head', $data);
         $this->load->view('menu', $data);
         $this->load->view('section/error');
     }
+
 }
